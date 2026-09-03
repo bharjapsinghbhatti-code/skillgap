5ff950a19d4aef214072b2a319b8c1a1a3149e8c
BHATTI05 <harjaap1212@gmail.com>
2026-09-03 18:15:59 +0530

    Allow deployed frontend CORS

--- api/main.py
+++ api/main.py
@@ -25,7 +25,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",`r`n        "https://skillgap-1-3fki.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
